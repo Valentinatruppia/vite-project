2805e20104da51b91a33080b8c0544405c3b5fb8
Valentinatruppia <valentinatruppia@gmail.com>
2023-11-29 08:53:00 +0100

    menu a tendina

--- src/App.svelte
+++ src/App.svelte
@@ -1,67 +1,48 @@
 <script>
 // @ts-nocheck
-
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
-
 	let url = "https://raw.githubusercontent.com/Valentinatruppia/vite-project/main/incidenti.json"
 	
 	import fetchStore from './fetch.js'
 	const [data, loading, error, get] = fetchStore(url)
+	import Menu from './lib/Menu.svelte'
+	
+	let name = 'world';
 </script>
 
-<button on:click={get}>
-	Fetch again
-</button>
-
-{#if $loading}
-	Loading: {$loading}
-{:else if $error}
-	Error: {$error}
-{:else}
-	<pre>{JSON.stringify($data, null, 2)}</pre>
-{/if}
 
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+  <h1>CRASH ANALYZER</h1>
+	<Menu />
+	<section class="content">
+		<h1>Hello {name}!</h1>
 
-  <div class="card">
-    <Counter />
-  </div>
+    <button on:click={get}>
+    Fetch again
+    </button>
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+    {#if $loading}
+      Loading: {$loading}
+    {:else if $error}
+      Error: {$error}
+    {:else}
+      <pre>{JSON.stringify($data, null, 2)}</pre>
+    {/if}
+	</section> 
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+
+	:global(body) {
+		margin: 0;
+		padding: 0;
+	}
+	main {
+		display: grid;
+		grid-template:
+			"nav content" min-content
+	}
+	.content {
+		grid-area: content;
+	}
 </style>
---
+++ src/lib/Menu.svelte
@@ -0,0 +1,93 @@
+
+<script>
+	import { fade } from 'svelte/transition';
+    import menu from '/src/assets/menu.png'
+	
+	const fadeIn = {
+		delay: 100,
+		duration: 200
+	}
+	
+	const fadeOut = {
+		delay: 0,
+		duration: 100
+	}
+	
+	let isExpanded = false;
+</script>
+
+<nav class:expanded={isExpanded}>
+	<button on:click={() => isExpanded = !isExpanded}>
+		{#if isExpanded}
+        <a>
+            <img src="menu" >
+        </a>
+		{:else}
+        <a>
+            <img src="menu" >
+        </a>
+		{/if}
+	</button>
+	<ul>
+		<li>🚀 {#if isExpanded}<span in:fade="{fadeIn}" out:fade="{fadeOut}">Home</span>{/if}</li>
+		<li>⭐️ {#if isExpanded}<span in:fade="{fadeIn}" out:fade="{fadeOut}">Statistiche</span>{/if}</li>
+		
+	</ul>
+	{#if isExpanded}
+	<section in:fade="{fadeIn}" out:fade="{fadeOut}" >
+		<hr />
+		<ul>
+			<h3>
+				Contatti:
+			</h3>
+			<li>valentinatruppia@gmail.com</li>
+			<li>3665436483</li>
+		</ul>
+	</section>
+	{/if}
+</nav>
+
+<style>
+	nav {
+		grid-area: nav;
+		height: 100vw;
+		background-color: #324754;
+		color: #A2B7C4;
+		transition: ease-out 200ms;
+		width: 60px;
+		overflow: hidden;
+	}
+
+	.expanded {
+		transition: ease-out 200ms;
+		width: 200px;
+	}
+	
+	ul {
+		list-style: none;
+		padding: 20px;
+		margin: 0;
+	}
+	
+	li {
+		width: 200px;
+	}
+	
+	h3 {
+		text-transform: uppercase;
+		margin: 0;
+		padding: 10px 0;
+	}
+	
+	hr {
+	  color: white;
+		width: 80%;
+	}
+	
+	button {
+		border: none;
+		background: none;
+		color: #A2B7C4;
+		text-transform: uppercase;
+	}
+</style>
