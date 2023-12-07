b670e39743fb6c0ae5b48d53e7d24513d8f5cab4
Valentinatruppia <valentinatruppia@gmail.com>
2023-12-07 21:02:06 +0100

    fix table and add json 2008

--- src/App.svelte
+++ src/App.svelte
@@ -1,49 +1,86 @@
 <script>
-// @ts-nocheck
-	let url = "https://raw.githubusercontent.com/Valentinatruppia/vite-project/main/incidenti.json"
-
-	import fetchStore from './fetch.js'
-	const [data, loading, error, get] = fetchStore(url)
 	import Menu from './lib/Menu.svelte'
-	import VolunteerCards from './Table/VolunteerCards.svelte';
-	import DataTable from './DataTable.svelte';
-
+	import DataTable from './Table/DataTable.svelte';
+	import DataTablePast from './Table/DataTablePast.svelte';
 </script>
-	<h1>CRASH ANALYZER</h1>
-<Menu />  	
-<VolunteerCards />
 
 <main>
+	<h1>CRASH ANALYZER</h1>
+
 	<section class="content">
-    <button on:click={get}>
-    Fetch again
-    </button>
+		<h2>Tabella Dati Incidenti</h2>
+		<Menu />
 
-    {#if $loading}
-      Loading: {$loading}
-    {:else if $error}
-      Error: {$error}
-    {:else}
-      <pre>{JSON.stringify($data, null, 2)}</pre>
-    {/if}
-
+		<div class="tables-container">
+			<DataTable />
+			<div class="table-separator"></div> <!-- Aggiunto un div separatore -->
+			<DataTablePast />
+		</div>
 	</section>
-	<h1>Tabella Dati Incidenti</h1>
-  <DataTable />
 </main>
 
 <style>
-
 	:global(body) {
 		margin: 0;
 		padding: 0;
+		font-family: 'Arial', sans-serif;
+		background-color: #f5f5f5; /* Background slightly lighter than white */
+		color: #333;
 	}
+
 	main {
-		text-align:center;
-		grid-template:
-			"nav content" min-content
+		text-align: center;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		min-height: 100vh;
 	}
+
+	h1 {
+		margin-bottom: 10px;
+		font-size: 2.5em;
+		color: #2e7d32; /* Green color */
+		text-shadow: 2px 2px 2px rgba(0, 0, 0, 0.1); /* Slight text shadow */
+	}
+
+
 	.content {
-		grid-area: content;
+		width: 100%;
+		max-width: 9000px; /* Set a maximum width for the content */
+		padding: 20px;
+		background-color: #fff;
+		box-shadow: 0 2px 4px rgba(66, 56, 56, 1); /* Slight box shadow */
+		border-radius: 20px;
+		margin-top: 20px; /* Aggiunto spazio sopra il contenuto */
+		margin-bottom: 20px; /* Aggiunto spazio sotto il contenuto */
+        margin-left: 20px;  /* Aggiunto margine sinistro */
+        margin-right: 20px; /* Aggiunto margine destro */
 	}
+    
+
+	h2 {
+		font-size: 1.8em;
+		color: #1565c0; /* Blue color */
+		margin-bottom: 20px;
+	}
+
+	.tables-container {
+		display: flex;
+		justify-content: space-between; /* Spazio uniforme tra le tabelle */
+		margin-top: 2px; /* Aumentato il margine superiore per più spazio */
+	}
+
+	.table-separator {
+		width: 20px; /* Larghezza del separatore */
+	}
+
+	/* Adjust width and height of the DataTable component */
+	.table-container {
+		flex: 0 0 48%; /* Larghezza massima del 48% per garantire uno spazio adeguato tra le tabelle */
+		overflow-x: auto;
+		max-height: 250px; /* Adjust the height as needed */
+	}
+
+	/* Add additional styles for Menu and DataTable components if needed */
 </style>
---
+++ src/Table/DataTablePast.svelte
@@ -0,0 +1,67 @@
+<script>
+    import { onMount } from "svelte";
+  
+    let dati = [];
+  
+    onMount(async () => {
+      // Carica i dati dal file
+      dati = await fetch("https://raw.githubusercontent.com/Valentinatruppia/vite-project/main/incidentipast.json").then((response) => response.json());
+    });
+  </script>
+  
+  <style>
+    body {
+      overflow: hidden; /* Impedisce lo scrolling sulla pagina principale */
+      margin: 0; /* Rimuove il margine predefinito */
+    }
+  
+    .tabella-container {
+      max-width: 95vw; /* Imposta la larghezza massima a 95% della larghezza della viewport */
+      max-height: 80vh; /* Imposta l'altezza massima a 80% dell'altezza della viewport */
+      overflow-y: auto;  /* Abilita lo scrolling verticale se necessario */
+      margin: 20px auto; /* Aggiunge margine per una migliore presentazione e centra la tabella */
+    }
+  
+    .tabella {
+      border-collapse: collapse;
+      width: 100%;
+    }
+  
+    .tabella th, .tabella td {
+      border: 2px solid black; /* Imposta i bordi a 2px di spessore e colore nero */
+      padding: 8px;
+      text-align: left;
+    }
+  
+    .tabella th {
+      background-color: #f2f2f2;
+      position: sticky;
+      top: 0;
+    }
+  </style>
+  
+  {#if dati.length > 0}
+    <div class="tabella-container">
+      <table class="tabella">
+        <thead>
+          <tr>
+            {#each Object.keys(dati[0]) as intestazione}
+              <th>{intestazione}</th>
+            {/each}
+          </tr>
+        </thead>
+        <tbody>
+          {#each dati as dato}
+            <tr>
+              {#each Object.values(dato) as cella}
+                <td>{cella}</td>
+              {/each}
+            </tr>
+          {/each}
+        </tbody>
+      </table>
+    </div>
+  {:else}
+    <p>Caricamento dati...</p>
+  {/if}
+  
